3fa45ecdd1b3fd71266ee992c698677cd4d5a042
zhuzhe <zhe.zhu1@pactera.com>
2018-06-05 14:01:22 +0800

    WebMvcConfig

---
+++ src/main/java/com/space/shiro/config/shiro/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.space.shiro.config.shiro;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author zhuzhe
+ * @date 2018/6/5 13:31
+ * @email 1529949535@qq.com
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    /*@Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MyInterceptor());
+    }*/
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/index").setViewName("index");
+
+        registry.addViewController("/testPage").setViewName("testPage");
+    }
+}
