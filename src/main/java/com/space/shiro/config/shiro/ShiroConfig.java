9b543a5528f90d1d659db5bc1b10d4164b716d52
zhuzhe <zhe.zhu1@pactera.com>
2018-06-05 09:11:00 +0800

    配置shiro

--- src/main/java/com/space/shiro/config/shiro/ShiroConfig.java
+++ src/main/java/com/space/shiro/config/shiro/ShiroConfig.java
@@ -2,11 +2,12 @@ package com.space.shiro.config.shiro;
 
 import com.space.shiro.shiro.AuthRealm;
 import com.space.shiro.shiro.CredentialsMatcher;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-import org.apache.shiro.mgt.SecurityManager;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -20,9 +21,15 @@ import java.util.LinkedHashMap;
  * @date 2018/6/4 15:55
  * @email 1529949535@qq.com
  */
+@Slf4j
 @Configuration
 public class ShiroConfig {
 
+    /**
+     * 配置ShiroFilter
+     * @param manager
+     * @return
+     */
     @Bean(name="shiroFilter")
     public ShiroFilterFactoryBean shiroFilter(@Qualifier("securityManager") SecurityManager manager) {
         ShiroFilterFactoryBean bean=new ShiroFilterFactoryBean();
@@ -31,28 +38,25 @@ public class ShiroConfig {
         bean.setLoginUrl("/login");
         bean.setSuccessUrl("/home");
         //配置访问权限
-        LinkedHashMap<String, String> filterChainDefinitionMap=new LinkedHashMap<>();
-        filterChainDefinitionMap.put("/login*", "anon"); //表示可以匿名访问
+        LinkedHashMap<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
+
+        // anon 表示可以匿名访问
+        filterChainDefinitionMap.put("/login*", "anon");
         filterChainDefinitionMap.put("/loginUser", "anon");
-        filterChainDefinitionMap.put("/client/test", "anon");
-        filterChainDefinitionMap.put("/assert/test", "anon");//添加白名单
-        filterChainDefinitionMap.put("/assert/get", "anon");//添加白名单
-        filterChainDefinitionMap.put("/assert/assertQuery", "anon");//添加白名单
-        filterChainDefinitionMap.put("/a", "anon");
-        filterChainDefinitionMap.put("/book/list", "anon");
         filterChainDefinitionMap.put("/logout*","anon");
-        filterChainDefinitionMap.put("/jsp/error.jsp*","anon");
-        filterChainDefinitionMap.put("/jsp/login.jsp*","authc");
-        filterChainDefinitionMap.put("/*", "authc");//表示需要认证才可以访问
-        filterChainDefinitionMap.put("/**", "authc");//表示需要认证才可以访问
+
+        //authc  表示需要认证才可以访问
+        filterChainDefinitionMap.put("/*", "authc");
+        filterChainDefinitionMap.put("/**", "authc");
         filterChainDefinitionMap.put("/*.*", "authc");
+
         bean.setFilterChainDefinitionMap(filterChainDefinitionMap);
         return bean;
     }
     //配置核心安全事务管理器
     @Bean(name="securityManager")
     public SecurityManager securityManager(@Qualifier("authRealm") AuthRealm authRealm) {
-        System.err.println("--------------shiro已经加载----------------");
+        log.info("--------------shiro已经加载----------------");
         DefaultWebSecurityManager manager=new DefaultWebSecurityManager();
         manager.setRealm(authRealm);
         return manager;
@@ -75,7 +79,7 @@ public class ShiroConfig {
     }
     @Bean
     public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator(){
-        DefaultAdvisorAutoProxyCreator creator=new DefaultAdvisorAutoProxyCreator();
+        DefaultAdvisorAutoProxyCreator creator = new DefaultAdvisorAutoProxyCreator();
         creator.setProxyTargetClass(true);
         return creator;
     }
