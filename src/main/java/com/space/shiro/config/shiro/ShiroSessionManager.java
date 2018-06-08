38cb46fadc982c1f7e218c9ac5cdad8067efb989
zhuzhe <zhe.zhu1@pactera.com>
2018-06-08 16:24:34 +0800

    自定义 ShiroSessionManager

--- src/main/java/com/space/shiro/config/shiro/ShiroSessionManager.java
+++ src/main/java/com/space/shiro/config/shiro/ShiroSessionManager.java
@@ -11,11 +11,12 @@ import java.io.Serializable;
 
 /**
  * 自定义 ShiroSessionManager类继承 DefaultWebSessionManager类，重写 getSessionId方法
+ *
  * @author zhuzhe
  * @date 2018/6/8 16:09
  * @email 1529949535@qq.com
  */
-public class ShiroSessionManager extends DefaultWebSessionManager{
+public class ShiroSessionManager extends DefaultWebSessionManager {
 
     private static final String AUTHORIZATION = "authToken";
 
@@ -27,18 +28,17 @@ public class ShiroSessionManager extends DefaultWebSessionManager{
      * 我们选择在ajax的请求头中传递sessionId，因此需要重写shiro获取sessionId的方式。
      */
 
-
     @Override
-    protected Serializable getSessionId(ServletRequest request, ServletResponse response){
+    protected Serializable getSessionId(ServletRequest request, ServletResponse response) {
         String id = WebUtils.toHttp(request).getHeader(AUTHORIZATION);
-        if(StringUtils.isEmpty(id)){
+        if (StringUtils.isEmpty(id)) {
             //如果没有携带id参数则按照父类的方式在cookie进行获取
             return super.getSessionId(request, response);
-        }else{
+        } else {
             //如果请求头中有 authToken 则其值为sessionId
-            request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID_SOURCE,REFERENCED_SESSION_ID_SOURCE);
-            request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID,id);
-            request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID_IS_VALID,Boolean.TRUE);
+            request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID_SOURCE, REFERENCED_SESSION_ID_SOURCE);
+            request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID, id);
+            request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID_IS_VALID, Boolean.TRUE);
             return id;
         }
     }
