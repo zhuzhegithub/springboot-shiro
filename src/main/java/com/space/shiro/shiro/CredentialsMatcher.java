dd3530d5fc2adc71ec8619821d4c7473792fdc76
zhuzhe <zhe.zhu1@pactera.com>
2018-06-08 16:42:12 +0800

    自定义的密码比较器

--- src/main/java/com/space/shiro/shiro/CredentialsMatcher.java
+++ src/main/java/com/space/shiro/shiro/CredentialsMatcher.java
@@ -6,7 +6,8 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authc.credential.SimpleCredentialsMatcher;
 
 /**
- * 权限用户密码校验类
+ * 自定义的密码比较器
+ *
  * @author zhuzhe
  * @date 2018/6/4 15:52
  * @email 1529949535@qq.com
@@ -16,11 +17,11 @@ public class CredentialsMatcher extends SimpleCredentialsMatcher {
     //校验
     @Override
     public boolean doCredentialsMatch(AuthenticationToken token, AuthenticationInfo info) {
-        UsernamePasswordToken utoken=(UsernamePasswordToken) token;
+        UsernamePasswordToken utoken = (UsernamePasswordToken) token;
         //获得用户输入的密码:(可以采用加盐(salt)的方式去检验)
         String inPassword = new String(utoken.getPassword());
         //获得数据库中的密码
-        String dbPassword=(String) info.getCredentials();
+        String dbPassword = (String) info.getCredentials();
         //进行密码的比对
         return this.equals(inPassword, dbPassword);
     }
