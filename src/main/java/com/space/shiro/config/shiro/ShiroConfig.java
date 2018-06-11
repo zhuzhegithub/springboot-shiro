c3fbd7c5b39d971c653d929f42723894a62f3d98
zhuzhe <zhe.zhu1@pactera.com>
2018-06-11 09:54:23 +0800

    ShiroConfig

--- src/main/java/com/space/shiro/config/shiro/ShiroConfig.java
+++ src/main/java/com/space/shiro/config/shiro/ShiroConfig.java
@@ -5,20 +5,14 @@ import com.space.shiro.shiro.AuthRealm;
 import com.space.shiro.shiro.CredentialsMatcher;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.mgt.SecurityManager;
-import org.apache.shiro.session.mgt.SessionManager;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.servlet.SimpleCookie;
-import org.crazycake.shiro.RedisCacheManager;
-import org.crazycake.shiro.RedisManager;
-import org.crazycake.shiro.RedisSessionDAO;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -35,14 +29,13 @@ import java.util.LinkedHashMap;
 @Configuration
 public class ShiroConfig {
 
-    @Autowired
-    private RedisProperties redisProperties;
-
-    /*配置ShiroFilter*/
+    /**
+     * 配置ShiroFilter
+     */
     @Bean(name = "shiroFilter")
-    public ShiroFilterFactoryBean shiroFilter(SecurityManager securityManager) {
+    public ShiroFilterFactoryBean shiroFilter(@Qualifier("securityManager") SecurityManager manager) {
         ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
-        bean.setSecurityManager(securityManager);
+        bean.setSecurityManager(manager);
         //配置登录的url和登录成功的url
         bean.setLoginUrl("/login");
 //        bean.setSuccessUrl("/index");
@@ -64,82 +57,37 @@ public class ShiroConfig {
         return bean;
     }
 
-    //配置核心安全控制层
+    /**
+     * 开启shiro aop注解支持,开启 权限注解
+     * Controller才能使用 @RequiresPermissions
+     */
     @Bean
-    public SecurityManager securityManager(AuthRealm authRealm, SessionManager sessionManager) {
-        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-        securityManager.setRealm(authRealm);
-        // 自定义session管理
-        securityManager.setSessionManager(sessionManager);
-        // 自定义缓存实现
-        //securityManager.setCacheManager(redisCacheManager);
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(@Qualifier("securityManager") SecurityManager manager) {
+        AuthorizationAttributeSourceAdvisor advisor = new AuthorizationAttributeSourceAdvisor();
+        advisor.setSecurityManager(manager);
+        return advisor;
+    }
+
+    //配置核心安全事务管理器
+    @Bean(name = "securityManager")
+    public SecurityManager securityManager(AuthRealm authRealm) {
+        DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
+        manager.setRealm(authRealm);
         //注入记住我管理器
-        // securityManager.setRememberMeManager(rememberMeManager);
-        return securityManager;
+//        manager.setRememberMeManager(rememberMeManager());
+        return manager;
     }
 
     //配置自定义的权限登录器
     @Bean
     public AuthRealm authRealm() {
         AuthRealm authRealm = new AuthRealm();
+        //配置自定义的密码比较器
         authRealm.setCredentialsMatcher(new CredentialsMatcher());
         return authRealm;
     }
 
-    // 配置session管理器
-    @Bean
-    public SessionManager sessionManager(RedisSessionDAO redisSessionDAO, RedisCacheManager redisCacheManager) {
-        ShiroSessionManager shiroSessionManager = new ShiroSessionManager();
-        //设置session过期时间为1小时(单位：毫秒)，默认为30分钟
-        shiroSessionManager.setGlobalSessionTimeout(60 * 60 * 1000);
-        //这里可以不设置。Shiro有默认的session管理。如果缓存为Redis则需改用Redis的管理
-        shiroSessionManager.setSessionDAO(redisSessionDAO);
-        shiroSessionManager.setCacheManager(redisCacheManager);
-        return shiroSessionManager;
-    }
-
-    /**
-     * RedisSessionDAO shiro sessionDao层的实现 通过redis
-     * 使用的是shiro-redis开源插件
-     */
-    @Bean
-    public RedisSessionDAO redisSessionDAO(RedisManager redisManager) {
-        RedisSessionDAO redisSessionDAO = new RedisSessionDAO();
-        redisSessionDAO.setRedisManager(redisManager);
-        return redisSessionDAO;
-    }
-
-    /**
-     * cacheManager 缓存 redis实现
-     * 使用的是shiro-redis开源插件
-     */
-    @Bean
-    public RedisCacheManager redisCacheManager(RedisManager redisManager) {
-        RedisCacheManager redisCacheManager = new RedisCacheManager();
-        redisCacheManager.setRedisManager(redisManager);
-        return redisCacheManager;
-    }
-
-    /**
-     * 配置shiro redisManager
-     * 使用的是shiro-redis开源插件
-     *
-     * @return
-     */
-    @Bean
-    public RedisManager redisManager() {
-        RedisManager redisManager = new RedisManager();
-        redisManager.setHost(redisProperties.getHost());
-        redisManager.setPort(redisProperties.getPort());
-        // redisManager.setExpire();
-        // redisManager.setTimeout();// 配置缓存过期时间
-        // redisManager.setPassword(redisProperties.getPassword());
-        return redisManager;
-    }
-
-    /**
-     * 管理shiro bean生命周期
-     */
+    /*管理shiro bean生命周期*/
     @Bean
     public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
         return new LifecycleBeanPostProcessor();
@@ -150,17 +98,6 @@ public class ShiroConfig {
         DefaultAdvisorAutoProxyCreator creator = new DefaultAdvisorAutoProxyCreator();
         creator.setProxyTargetClass(true);
         return creator;
-    }
-
-    /**
-     * 开启shiro aop注解支持,开启 权限注解
-     * Controller才能使用 @RequiresPermissions
-     */
-    @Bean
-    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(@Qualifier("securityManager") SecurityManager manager) {
-        AuthorizationAttributeSourceAdvisor advisor = new AuthorizationAttributeSourceAdvisor();
-        advisor.setSecurityManager(manager);
-        return advisor;
     }
 
     /*remember me*/
@@ -175,8 +112,6 @@ public class ShiroConfig {
 
     /**
      * cookie管理对象;
-     *
-     * @return CookieRememberMeManager
      */
     @Bean
     public CookieRememberMeManager rememberMeManager() {
@@ -185,7 +120,9 @@ public class ShiroConfig {
         return cookieRememberMeManager;
     }
 
-    /*shiro方言  支持shiro标签*/
+    /**
+     * shiro方言  支持shiro标签
+     */
     @Bean
     public ShiroDialect shiroDialect() {
         return new ShiroDialect();
